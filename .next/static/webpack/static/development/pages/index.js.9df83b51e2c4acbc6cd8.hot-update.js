256ba3be6280726aeab69d9e87733407bf5d255e
Pedre <pviljoen@fullfacing.com>
2018-12-05 09:54:56 +0200

    Init commit :beers:

---
+++ .next/static/webpack/static/development/pages/index.js.9df83b51e2c4acbc6cd8.hot-update.js
@@ -0,0 +1,63 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/pedre/Documents/Work/init/meetups/next-js-setup/pages/index.js";
+
+var containerStyle = {
+  display: 'flex',
+  margin: 20,
+  padding: 20,
+  border: '1px solid #DDD',
+  alignContent: 'center',
+  justifyContent: 'center'
+};
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    style: containerStyle,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 13
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 14
+    },
+    __self: this
+  }, "init - NextJS"));
+});
+    (function (Component, route) {
+      if(!Component) return
+      if (false) {}
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__.default : (module.exports.default || module.exports), "/")
+  
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../node_modules/webpack/buildin/harmony-module.js */ "./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.9df83b51e2c4acbc6cd8.hot-update.js.map
---
+++ pages/index.js
@@ -0,0 +1,18 @@
+const containerStyle = {
+  display: 'flex',
+  margin: 20,
+  padding: 20,
+  border: '1px solid #DDD',
+  alignContent: 'center',
+  justifyContent: 'center'
+}
+
+export default () => {
+  return(
+    <div style={containerStyle}>
+      <p>
+        init - NextJS
+      </p>
+    </div>
+  )
+}
